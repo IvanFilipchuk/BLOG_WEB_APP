c761ce3427892deea35d1bab924f201a082522da
Ivan Filipchuk <ivanflpck@gmial.com>
2024-04-12 20:27:15 +0200

    Database config

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+import e from 'express';
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+
+mongoose
+.connect('mongodb+srv://ivanfilipchukp:lR6dldV0WctmVhIi@cluster0.cw2dupb.mongodb.net/')
+.then(()=>console.log('DB OK'))
+.catch((err) => console.log('DB error', err))
+
 
 const app = express();
 
 app.get('/', (req, res) =>  {
-    res.send('Hello Ivaneneko');
+    res.send('Hello Ivan');
+});
+
+app.use(express.json());
+
+app.post('/auth/login',(req, res)=>{
+    console.log(req.body);
+    const token = jwt.sign({
+        name: req.body.name,
+        lastname:req.body.lastname,
+    },
+    'hs256',
+);
+
+    res.json({
+        success: true,
+        token,
+    });
 });
 
 app.listen(4444, (err) => {
