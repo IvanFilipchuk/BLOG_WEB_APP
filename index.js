fd2118057542211cf871375abe5bc3ee27df8833
Ivan Filipchuk <ivanflpck@gmial.com>
2024-04-13 02:44:55 +0200

    Add login and register

--- index.js
+++ index.js
@@ -18,26 +18,90 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', async(req, res)=>{
+    try{
+        const user = await UserModel.findOne({email: req.body.email});
+
+        if(!user){
+            return res.status(404).json({
+                message: 'User is not found',
+            });
+        }
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+       
+        if(!isValidPass){
+            return res.status(404).json({
+                message: 'Login or password is incorrect',
+            });
+        }
+
+        const token = jwt.sign({
+            _id: user._id,
+        },
+        'secure123',
+        {
+            expiresIn: '30d'
+    
+        },
+        );
+        const {passwordHash, ...userData} = user._doc; 
+        res.json({
+            ...userData,
+            token,
+        });
+
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+        message: 'Failed authentication!',
+    });
+    }
+});
+
 app.post('/auth/register', registerValidator, async (req, res) =>{
+   try{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json(errors.array());
     }
+
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+    const hash = await bcrypt.hash(password, salt);
 
     const doc = new UserModel({
         email: req.body.email,
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         avatarUrl: req.body.avatar,
-        passwordHash,
+        passwordHash: hash,
 
     });
 
     const user = await doc.save();
-    res.json(user);
+
+    const token = jwt.sign({
+        _id: user._id,
+    },
+    'secure123',
+    {
+        expiresIn: '30d'
+
+    },
+);
+    const {passwordHash, ...userData} = user._doc; 
+    res.json({
+        ...userData,
+        token,
+    });
+
+   }catch(err){
+    console.log(err);
+    res.status(500).json({
+        message: 'Failed registration!',
+    });
+   }
 });
 
 app.listen(4444, (err) => {
